0bdf4e5cc987ec826283087a6c9c6bbf9184fedc
Sujeesha <129931462+Sujeesha@users.noreply.github.com>
2023-12-14 14:09:02 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,98 +1,25 @@
-import React, { useState } from "react";
-import { NavLink, Route, Routes, useLocation } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import BackgroundWithText from "./components/BackgroundWithText";
-import Add from "./components/Add";
-import Notfound from "./components/Notfound";
 import Myhome from "./components/Myhome";
-import Update from "./components/Update";
-import Delete from "./components/Delete";
-import Footer from "./components/Footer";
-import Song16Page from "./components/Song16Page"; // Import the Song16Page component
-
+import BackgroundWithText from "./components/BackgroundWithText";
+import Login from "./components/Login";
+import SignupForm from "./components/SignupForm";
+import Footer from "./components/Footer"; // Import the Footer component
 function App() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const location = useLocation();
-  const username = new URLSearchParams(location.search).get("username");
-
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
-    // Handle search functionality here
-    console.log("Search submitted:", searchQuery);
-  };
-
   return (
-    <div>
-      <nav className="navbar navbar-expand navbar-light bg-dark fixed-top">
-        <ul className="nav">
-          <li className="nav-item">
-            <NavLink className="nav-link active" to="/" aria-current="page">
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/Add">
-              Add
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/Update">
-              Update
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/Delete">
-              Delete
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <form className="form-inline" onSubmit={handleSearchSubmit}>
-              <div className="input-group">
-                <input
-                  className="form-control"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                  value={searchQuery}
-                  onChange={handleSearchChange}
-                />
-                <div className="input-group-append">
-                  <button className="btn btn-outline-success" type="submit">
-                    <FontAwesomeIcon icon={faSearch} />
-                  </button>
-                </div>
-              </div>
-            </form>
-          </li>
-        </ul>
-        {/* Display the welcome message on the right side */}
-        {username && (
-          <div className="nav ml-auto">
-            <p className="nav-link">Welcome, {username}!</p>
-          </div>
-        )}
-      </nav>
-
-      <div className="container" style={{ marginTop: "60px" }}>
+    <Router>
+      <div>
+        {/* Routes for both login and signup */}
         <Routes>
           <Route path="/" element={<Myhome />} />
-          <Route path="/Add" element={<Add />} />
-          <Route path="/Update" element={<Update />} />
-          <Route path="/Delete" element={<Delete />} />
-          <Route path="/song-16-page/:songId" element={<Song16Page />} />
-          <Route path="*" element={<Notfound />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignupForm />} />
         </Routes>
         <BackgroundWithText />
       </div>
-
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.css";
-import { UserProvider } from "./components/userContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <UserProvider> {/* Wrap App with UserProvider */}
-        <App />
-      </UserProvider>
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
